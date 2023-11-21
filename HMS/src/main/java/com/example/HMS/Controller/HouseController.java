780ba5277ed043429163ad5141270b475555379e
karth <karthiknoone@gmail.com>
2023-11-20 21:57:22 -0500

    added name

--- HMS/src/main/java/com/example/HMS/Controller/HouseController.java
+++ HMS/src/main/java/com/example/HMS/Controller/HouseController.java
@@ -24,6 +24,7 @@ import com.example.HMS.Repository.HmsRepository;
 // which acts connecting request from website to back-end code
 // and also performs the operation based on web request 
 
+	//karthik
 @Controller
 public class HouseController {
 

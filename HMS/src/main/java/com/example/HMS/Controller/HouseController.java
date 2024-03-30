ac7fa0bcaaa80f9f6f4a09994c49622c0a97598e
karth <karthiknoone@gmail.com>
2024-03-29 22:34:59 -0400

    added some changes

--- HMS/src/main/java/com/example/HMS/Controller/HouseController.java
+++ HMS/src/main/java/com/example/HMS/Controller/HouseController.java
@@ -30,7 +30,7 @@ public class HouseController {
 
 	ModelAndView mv = new ModelAndView();
 	@Autowired
-	HmsRepository houserepo;
+	HmsRepository houserepo  s;
 
 	/*
 	 * @RequestMapping("home") public String home() { return "home.html"; }
